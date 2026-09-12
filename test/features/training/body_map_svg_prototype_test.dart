2081384f59b0b6cb557f31165bf4e0a15df66de1
nl07wq <nl07wq@gmail.com>
2026-09-12 11:10:43 +0900

    refine: simplify body map svg geometry

--- test/features/training/body_map_svg_prototype_test.dart
+++ test/features/training/body_map_svg_prototype_test.dart
@@ -67,4 +67,66 @@ void main() {
     expect(frontDocument.paths['front-body'], isNotNull);
     expect(backDocument.paths['back-body'], isNotNull);
   });
+
+  testWidgets(
+    'keeps the simplified bodies and bilateral regions geometrically sane',
+    (tester) async {
+      final front = await loadSvgBodyMap(SvgBodyMapSide.front);
+      final back = await loadSvgBodyMap(SvgBodyMapSide.back);
+      final frontBody = front.paths['front-body']!.getBounds();
+      final backBody = back.paths['back-body']!.getBounds();
+
+      expect(frontBody.width, greaterThan(80));
+      expect(frontBody.height, greaterThan(250));
+      expect(backBody.width, closeTo(frontBody.width, .01));
+      expect(backBody.height, closeTo(frontBody.height, .01));
+
+      for (final document in [front, back]) {
+        for (final id in document.paths.keys) {
+          final bounds = document.paths[id]!.getBounds();
+          expect(bounds.width, greaterThan(0), reason: id);
+          expect(bounds.height, greaterThan(0), reason: id);
+        }
+      }
+
+      _expectMirrorPairs(front, const [
+        ('front-chest-left', 'front-chest-right'),
+        ('front-shoulder-left', 'front-shoulder-right'),
+        ('front-biceps-left', 'front-biceps-right'),
+        ('front-forearm-left', 'front-forearm-right'),
+        ('front-quadriceps-left', 'front-quadriceps-right'),
+      ]);
+      _expectMirrorPairs(back, const [
+        ('back-lats-left', 'back-lats-right'),
+        ('back-shoulder-left', 'back-shoulder-right'),
+        ('back-triceps-left', 'back-triceps-right'),
+        ('back-forearm-left', 'back-forearm-right'),
+        ('back-glutes-left', 'back-glutes-right'),
+        ('back-hamstrings-left', 'back-hamstrings-right'),
+        ('back-calves-left', 'back-calves-right'),
+      ]);
+    },
+  );
+}
+
+void _expectMirrorPairs(
+  SvgBodyMapDocument document,
+  List<(String, String)> pairs,
+) {
+  for (final (leftId, rightId) in pairs) {
+    final left = document.paths[leftId]!.getBounds();
+    final right = document.paths[rightId]!.getBounds();
+    expect(left.width, closeTo(right.width, .01), reason: '$leftId width');
+    expect(left.height, closeTo(right.height, .01), reason: '$leftId height');
+    expect(
+      left.center.dx + right.center.dx,
+      closeTo(200, .01),
+      reason: '$leftId center',
+    );
+    expect(
+      left.center.dy,
+      closeTo(right.center.dy, .01),
+      reason: '$leftId top',
+    );
+  }
 }
